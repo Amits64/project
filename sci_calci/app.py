5b8437246f79f2f20a3416334422a81a3afbd887
Amits64 <135766785+Amits64@users.noreply.github.com>
2023-10-21 16:30:53 +0530

    Update app.py

--- sci_calci/app.py
+++ sci_calci/app.py
@@ -2,19 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Define a constant for the template name
+INDEX_TEMPLATE = 'index.html'
+
 @app.route('/')
 def calculator():
-    return render_template('index.html')
+    return render_template(INDEX_TEMPLATE)
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
     try:
         expression = request.form['expression']
         result = eval(expression)
-        return render_template('index.html', result=result)
-    except:
-        return render_template('index.html', result="Error")
+        return render_template(INDEX_TEMPLATE, result=result)
+    except Exception as e:
+        # Specify an exception class and handle the error
+        return render_template(INDEX_TEMPLATE, result=f"Error: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
